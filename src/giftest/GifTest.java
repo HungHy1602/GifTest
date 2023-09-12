b30abf7399fa43e283586f269858986478d89ec4
HungHy1602 <leconghung160202@gmail.com>
2023-09-12 14:55:43 +0700

    The First Time Push Code Into Gif

---
+++ src/giftest/GifTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package giftest;
+
+/**
+ *
+ * @author ASUS
+ */
+public class GifTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("The First Time Use Gif");
+    }
+    
+}
